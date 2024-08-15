6a85d3dbffbeb9b0c1f3ea047833e868eed87e68
VitinhoReiDelas7 <muudiskibro@gmail.com>
2024-08-14 22:00:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from cotacoes.routes import cotacoes_bp  # Importando o Blueprint
 from flask import Flask, render_template, jsonify # Importe o modulo jsonify
 from weather.routes import weather_bp  # Importando o Blueprint
-
+from asgiref.wsgi import WsgiToAsgi
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "sua_chave_secreta"  # Substitua por uma chave secreta segura
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///livros.db"
@@ -305,7 +305,7 @@ def dashboard():
 # Registrando o Blueprint
 app.register_blueprint(cotacoes_bp)
 app.register_blueprint(weather_bp)
-
+asgi_app = WsgiToAsgi(app)
 if __name__ == "__main__":
     app.run(debug=True)
 
